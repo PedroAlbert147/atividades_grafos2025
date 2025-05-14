bbd6c5a1f9fd21dfc4be90821d840f09084c957c
PedroAlbert147 <152289606+PedroAlbert147@users.noreply.github.com>
2025-05-14 06:02:42 -0300

    Add files via upload

--- meu_grafo_lista_adj_nao_dir.py
+++ meu_grafo_lista_adj_nao_dir.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 from bibgrafo.grafo_lista_adj_nao_dir import GrafoListaAdjacenciaNaoDirecionado
 from bibgrafo.grafo_errors import *
 
@@ -11,14 +13,46 @@ class MeuGrafo(GrafoListaAdjacenciaNaoDirecionado):
         Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
         :return: Um objeto do tipo set que contém os pares de vértices não adjacentes
         '''
-        pass # Apague essa instrução e inicie seu código aqui
+
+        adjacentes = []
+        todasrelacoes = []
+
+        for verticeA in self.vertices:
+            for verticeB in self.vertices:
+                if verticeA.rotulo != verticeB.rotulo:
+                    #print(verticeA.rotulo,verticeB.rotulo)
+                    if verticeA.rotulo < verticeB.rotulo:
+                        todasrelacoes.append(verticeA.rotulo + "-" + verticeB.rotulo)
+                    else:
+                        todasrelacoes.append(verticeB.rotulo + "-" + verticeA.rotulo)
+        for aresta in self.arestas.values():
+
+            if aresta.v1.rotulo < aresta.v2.rotulo:
+                adjacentes.append(aresta.v1.rotulo + "-" + aresta.v2.rotulo)
+            else:
+                adjacentes.append(aresta.v2.rotulo + "-" + aresta.v1.rotulo)
+
+        #print(sorted(todasrelacoes))
+        #print(sorted(adjacentes))
+        todasrelacoes = set(todasrelacoes)
+        adjacentes = set(adjacentes)
+        naoadjacentes = todasrelacoes - adjacentes
+        #print(naoadjacentes)
+        return naoadjacentes
+
+
+
 
     def ha_laco(self):
         '''
         Verifica se existe algum laço no grafo.
         :return: Um valor booleano que indica se existe algum laço.
         '''
-        pass
+        for aresta in self.arestas.values():
+            if aresta.v1 == aresta.v2:
+                return True
+        return False
+
 
     def grau(self, V=''):
         '''
@@ -27,14 +61,33 @@ class MeuGrafo(GrafoListaAdjacenciaNaoDirecionado):
         :return: Um valor inteiro que indica o grau do vértice
         :raises: VerticeInvalidoError se o vértice não existe no grafo
         '''
-        pass
-
+        if self.existe_rotulo_vertice(V) == False:
+            raise VerticeInvalidoError
+        grau = 0
+        for aresta in self.arestas.values():
+            if aresta.v1.rotulo == V:
+                grau = grau + 1
+            if aresta.v2.rotulo == V:
+                grau = grau + 1
+        return grau
     def ha_paralelas(self):
         '''
         Verifica se há arestas paralelas no grafo
         :return: Um valor booleano que indica se existem arestas paralelas no grafo.
         '''
-        pass
+        arestas = []
+
+        for aresta in self.arestas.values():
+            if aresta.v1.rotulo < aresta.v2.rotulo:
+                atual = aresta.v1.rotulo+aresta.v2.rotulo
+            else:
+                atual = aresta.v1.rotulo+aresta.v2.rotulo
+            if atual in arestas:
+                return True
+            else:
+                arestas.append(atual)
+        return False
+
 
     def arestas_sobre_vertice(self, V):
         '''
@@ -43,11 +96,33 @@ class MeuGrafo(GrafoListaAdjacenciaNaoDirecionado):
         :return: Uma lista os rótulos das arestas que incidem sobre o vértice
         :raises: VerticeInvalidoException se o vértice não existe no grafo
         '''
-        pass
+        arestas = set()
+        if not self.existe_rotulo_vertice(V):
+            raise VerticeInvalidoError
+        for aresta in self.arestas.values():
+            if aresta.v1.rotulo == V or aresta.v2.rotulo == V:
+                arestas.add(aresta.rotulo)
+        return arestas
 
     def eh_completo(self):
         '''
         Verifica se o grafo é completo.
         :return: Um valor booleano que indica se o grafo é completo
         '''
-        pass
+
+        primoteste = False
+        for vertice in self.vertices:
+            grau = self.grau(vertice.rotulo)
+            if primoteste:
+                if grau != antegrau:
+                    return False
+            else:
+                primoteste = True
+            antegrau = grau
+        if self.ha_laco():
+            return False
+        if self.ha_paralelas():
+            return False
+        if self.vertices_nao_adjacentes():
+            return False
+        return True
